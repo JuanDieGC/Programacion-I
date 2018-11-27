5babb49b7e59660871e040a8737c84657b497642
JuanDieGC <41926462+JuanDieGC@users.noreply.github.com>
2018-11-26 21:04:20 -0400

    Add files via upload
    cambios en diseño de los caracteres del sapito, inicialización de un coche, cambio en uso de estructuras por simples variables.

--- main.cpp
+++ main.cpp
@@ -1,137 +1,193 @@
 #include <iostream>
 #include <fstream>
-#include <bits/stdc++.h>
-#include <conio.h>
 #include<windows.h>
+#include <string.h>
+#include <conio.h>
+#define tam 100
+#include <bits/stdc++.h>
 using namespace std;
-typedef struct{
-    int posx;
-    int posy;
-}tiposapito;
-typedef struct{
-    int ncarriles;
-    int ancho;
-    tiposapito sapito;
-}tipoautopista;
-typedef struct{
-    int listadecoches;
-    int velocidad;
-    int espacio;
-}tipocarril;
-typedef struct{
-    char tipomovil;
-    int posx;
-    int posy;
-}tipocoche;
-typedef struct{
-    int posx;
-    int posy;
-    int tiempo;
-    int tipcomida;
-}tipocomida;
-void gotoxy(int x,int y)
+void gotoxy(short x, short y)
 {
-    HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD dwPos;
-    dwPos.X=x;
-    dwPos.Y=y;
-    SetConsoleCursorPosition(hcon, dwPos);
+   COORD pos = { x, y };
+   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
-void color(int x)
+
+void color(WORD col)
 {
-   SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),x);
+   SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),col);
 }
-void inicializar_autopista(tipoautopista autopista)
+
+void aleatorio(int*num)
+{
+    *num=rand()%100;
+}
+void dibujar_sapito(int posrx,int posry)
+{
+    color(10);
+    char a=187,b=153,f=240,c=201,d=188,e=200;
+    gotoxy(posrx,posry);
+    cout<<e<<b<<b<<d;
+    gotoxy(posrx,posry+1);
+    cout<<c<<f<<f<<a<<endl;
+}
+void borrar_sapito(int posrx,int posry)
+{
+    char a=' ';
+    gotoxy(posrx,posry);
+    cout<<a<<a<<a<<a;
+    gotoxy(posrx,posry+1);
+    cout<<a<<a<<a<<a<<endl;
+}
+
+void inicializar_autopista(int largo, int ancho)
 {
     gotoxy(0,0);
-    char a=219;
-    color(8);
-    for(int x=0;x<autopista.ancho;x++)
+    char a=174;
+    int ancho1=largo*4;
+    int altura1=ancho*4;
+    color(3);
+    for(int x=0;x<ancho1;x++)
         cout<<a;
     color(7);
     gotoxy(0,1);
-    for(int x=0;x<autopista.ancho;x++)
+    for(int x=0;x<ancho1;x++)
         cout<<a;
     gotoxy(0,2);
     color(2);
-    for(int y=0;y<autopista.ncarriles;y++)
+    for(int y=0;y<ancho1;y++)
         cout<<a;
-    gotoxy(0,autopista.ncarriles);
+    gotoxy(0,altura1+7);
     color(10);
-    for(int x=0;x<autopista.ncarriles;x++)
+    for(int x=0;x<altura1;x++)
         cout<<a;
-    gotoxy(0,autopista.ncarriles);
+    gotoxy(0,altura1+6);
     color(7);
-    for(int x=0;x<autopista.ancho;x++)
+    for(int x=0;x<ancho1;x++)
         cout<<a;
 }
-void borrar_sapito(tiposapito sapito)
-{
-    char a=' ';
-    gotoxy(sapito.posx,sapito.posy);
-    cout<<a<<a<<a<<a;
-    gotoxy(sapito.posx,sapito.posy+1);
-    cout<<a<<a<<a<<a<<endl;
-}
-void inicializar_sapito(tiposapito sapito)
-{
-    color(10);
-    char a=187,b=220,f=223,c=201,d=188,e=200;
-    gotoxy(sapito.posx,sapito.posy);
-    cout<<e<<b<<b<<d;
-    gotoxy(sapito.posx,sapito.posy+1);
-    cout<<c<<f<<f<<a<<endl;
-}
-void actualizar_sapito(tiposapito sapito)
-{
-    color(10);
-    char a=187,b=220,f=223,c=201,d=188,e=200;
-    gotoxy(sapito.posx,sapito.posy);
-    cout<<e<<b<<b<<d;
-    gotoxy(sapito.posx,sapito.posy+1);
-    cout<<c<<f<<f<<a<<endl;
-}
-void inicializar_comida(tipocomida comida)
+void dibujar_autitos(int posax,int posay)
 {
 
+    char a=219,b=220,c=226;
+
+    color(4);
+    gotoxy(posax,posay+1);
+    cout<<b<<a<<a<<a<<a<<a<<a<<a<<a<<b;
+    gotoxy(posax,posay+2);
+    cout<<a;
+    color(8);
+    cout<<b<<b;
+    color(4);
+    cout<<a<<a<<a<<a;
+    color(8);
+    cout<<b<<b;
+    color(4);
+    cout<<a<<c;
+    color(7);
+    gotoxy(posax+2,posay+3);
+    cout<<c<<c<<"   "<<c<<c;
 }
-int main()
+void eliminar_auto(int posax,int posay)
 {
-    tipoautopista autopista;
-    tiposapito sapito;
-    sapito.posx=12,sapito.posy=24;
-    autopista.ancho=24,autopista.ncarriles=28;
-    inicializar_autopista(autopista);
-    inicializar_sapito(sapito);
-    while(sapito.posy!=2)
-    {
-        if(_kbhit())
+    char a=' ';
+    gotoxy(posax+2,posay);
+    gotoxy(posax,posay+1);
+    cout<<a<<a<<a<<a<<a<<a<<a<<a<<a<<a;
+    gotoxy(posax,posay+2);
+    cout<<a;
+    color(8);
+    cout<<a<<a;
+    cout<<a<<a<<a<<a;
+    color(8);
+    cout<<a<<a;
+    cout<<a<<a;
+    color(7);
+    gotoxy(posax+2,posay+3);
+    cout<<a<<a<<"   "<<a<<a;
+}
+
+void Movimiento_Naranja(int *posrx,int *posry)
+{
+     if(_kbhit())
     {
         char tecla=getch();
         if(tecla=='w'){
-                borrar_sapito(sapito);
-                sapito.posy=sapito.posy-4;
-                actualizar_sapito(sapito);
+                borrar_sapito(*posrx,*posry);
+                *posry=*posry-1;
+
         }
         if(tecla=='s'){
-                borrar_sapito(sapito);
-                sapito.posy=sapito.posy+4;
-                actualizar_sapito(sapito);
+                borrar_sapito(*posrx,*posry);
+                *posry=*posry+1;
+
         }
         if(tecla=='d'){
-                borrar_sapito(sapito);
-                sapito.posx=sapito.posx+4;
-                actualizar_sapito(sapito);
+                borrar_sapito(*posrx,*posry);
+                *posrx=*posrx+1;
+
         }
         if(tecla=='a'){
-                borrar_sapito(sapito);
-                sapito.posx=sapito.posx-4;
-                actualizar_sapito(sapito);
+                borrar_sapito(*posrx,*posry);
+                *posrx=*posrx-1;
+
     }
     }
+}
+void choque(int posrx,int posry,int posax,int posay,int *muerte,int *vidas)
+{
+    int k=0,j=0,l=0,m=0;
+
+        while(j<14&&k<13&&l<4&&m<3)
+        {
+            if(((posax+k)==posrx)&&((posay+l)==posry))
+            {
+                *muerte=1;
+                *vidas=*vidas-1;
+            }
+            j++;
+            k++;
+            if(l!=3&&k==13){
+                l++;
+                k=0;
+            }
+            if(m!=2&&j==14){
+                m++;
+                j=0;
+            }
+        }
+}
+
+void avanzar_posiciones_auto(int *posx,int *posy)
+{
+    dibujar_autitos(*posx,*posy);
+    Sleep(50);
+    eliminar_auto(*posx,*posy);
+    *posx+=1;
+    dibujar_autitos(*posx,*posy);
+    if(*posx>=90)
+    {
+        eliminar_auto(*posx,*posy);
+        *posx=0;
+        dibujar_autitos(*posx,*posy);
+    }
+}
+int main()
+{
+    int ancho=25,largo=10;
+    int posax=5,posay=3;
+    int vidas=3,muerte,numAut=0,limite=ancho,reloj,nivel=1;
+    int fin=0,aux=0;
+    int posrx=(ancho*4)/2,posry=(largo*4)+4;
+    inicializar_autopista(ancho,largo);
+    while(fin!=1 && posry!=2)
+    {
+            muerte=0;
+            reloj=1;
+            dibujar_sapito(posrx,posry);
+                avanzar_posiciones_auto(&posax,&posay);
+            Movimiento_Naranja(&posrx,&posry);
+            reloj++;
     }
     system("CLS");
-    gotoxy(25,35);
-    cout<<"llego a la meta"<<endl;
-    return 0;
+    cout<<"GANASTE"<<endl;
 }
